d95354bdab1b505d384712a3129626f4e402297b
Omescobell <omescobell@gmail.com>
2026-04-14 10:33:40 -0600

    cambio de estilos

--- fronted/ETE-Tech/src/app/service/carrito.service.ts
+++ fronted/ETE-Tech/src/app/service/carrito.service.ts
@@ -40,28 +40,120 @@ export class CarritoService {
     }
 
     exportarXML() {
-        //Estructura XML
-        let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<tiquet>\n`;
+        const totalNum = this.getTotal();
+        // Simulando que el precio de venta ya incluye el 16% de IVA
+        const subtotalNum = totalNum / 1.16;
+        const ivaNum = totalNum - subtotalNum;
+
+        const totalStr = totalNum.toFixed(2);
+        const subTotalStr = subtotalNum.toFixed(2);
+        const ivaStr = ivaNum.toFixed(2);
+
+        const fecha = new Date().toISOString().split('.')[0]; // YYYY-MM-DDTHH:mm:ss
+        
+        // Estructura XML CFDI 4.0 (Simulada para demostración)
+        let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<cfdi:Comprobante 
+    xmlns:cfdi="http://www.sat.gob.mx/cfd/4" 
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+    xsi:schemaLocation="http://www.sat.gob.mx/cfd/4 http://www.sat.gob.mx/sitio_internet/cfd/4/cfdv40.xsd" 
+    Version="4.0" 
+    Serie="A" 
+    Folio="12345" 
+    Fecha="${fecha}" 
+    Sello="MockSelloDePruebaCfdi40ParaDemostracion==" 
+    FormaPago="01" 
+    NoCertificado="00001000000500000000" 
+    Certificado="MockCertificadoDePrueba=" 
+    CondicionesDePago="Contado" 
+    SubTotal="${subTotalStr}" 
+    Moneda="MXN" 
+    TipoCambio="1" 
+    Total="${totalStr}" 
+    TipoDeComprobante="I" 
+    Exportacion="01" 
+    MetodoPago="PUE" 
+    LugarExpedicion="12345">
+    
+    <cfdi:Emisor 
+        Rfc="EKU9003173C9" 
+        Nombre="TU EMPRESA S.A DE C.V" 
+        RegimenFiscal="601"/>
+        
+    <cfdi:Receptor 
+        Rfc="XAXX010101000" 
+        Nombre="PUBLICO EN GENERAL" 
+        DomicilioFiscalReceptor="12345" 
+        RegimenFiscalReceptor="616" 
+        UsoCFDI="S01"/>
+        
+    <cfdi:Conceptos>
+`;
 
         for (const producto of this._listaCarrito()) {
-            xml += `  <producto>\n`;
-            xml += `    <id>${producto.id_producto}</id>\n`;
-            xml += `    <nombre>${this.escapeXML(producto.nombre)}</nombre>\n`;
-            xml += `    <categoria>${this.escapeXML(producto.categoria)}</categoria>\n`;
+            const precioVenta = producto.precio_venta;
+            const subtotalProd = precioVenta / 1.16;
+            const ivaProd = precioVenta - subtotalProd;
+
+            const precioVentaStr = precioVenta.toFixed(2);
+            const subtotalProdStr = subtotalProd.toFixed(2);
+            const ivaProdStr = ivaProd.toFixed(2);
+
+            const descripcion = producto.descripcion 
+                ? `${this.escapeXML(producto.nombre)} - ${this.escapeXML(producto.descripcion)}`
+                : this.escapeXML(producto.nombre);
+                
+            xml += `        <cfdi:Concepto 
+            ClaveProdServ="01010101" 
+            NoIdentificacion="${producto.id_producto}" 
+            Cantidad="1" 
+            ClaveUnidad="H87" 
+            Unidad="Pieza" 
+            Descripcion="${descripcion}" 
+            ValorUnitario="${subtotalProdStr}" 
+            Importe="${subtotalProdStr}" 
+            ObjetoImp="02">
+            
+            <cfdi:Impuestos>
+                <cfdi:Traslados>
+                    <cfdi:Traslado Base="${subtotalProdStr}" Impuesto="002" TipoFactor="Tasa" TasaOCuota="0.160000" Importe="${ivaProdStr}"/>
+                </cfdi:Traslados>
+            </cfdi:Impuestos>
+
+            <producto>
+                <id>${producto.id_producto}</id>
+                <nombre>${this.escapeXML(producto.nombre)}</nombre>
+                <categoria>${this.escapeXML(producto.categoria)}</categoria>\n`;
             if (producto.descripcion) {
-                xml += `    <descripcion>${this.escapeXML(producto.descripcion)}</descripcion>\n`;
+                xml += `                <descripcion>${this.escapeXML(producto.descripcion)}</descripcion>\n`;
             }
-            xml += `    <precio>${producto.precio_venta}</precio>\n`;
-            xml += `  </producto>\n`;
+            xml += `                <precio>${precioVentaStr}</precio>
+            </producto>
+        </cfdi:Concepto>\n`;
         }
-        xml += `  <total>${this.getTotal()}</total>\n`;
-        xml += `</tiquet>`;
+
+        xml += `    </cfdi:Conceptos>
+
+    <cfdi:Impuestos TotalImpuestosTrasladados="${ivaStr}">
+        <cfdi:Traslados>
+            <cfdi:Traslado Base="${subTotalStr}" Impuesto="002" TipoFactor="Tasa" TasaOCuota="0.160000" Importe="${ivaStr}"/>
+        </cfdi:Traslados>
+    </cfdi:Impuestos>
+
+    <cfdi:Addenda>
+        <TotalesFactura>
+            <Subtotal>${subTotalStr}</Subtotal>
+            <IVA_16>${ivaStr}</IVA_16>
+            <TotalPago>${totalStr}</TotalPago>
+        </TotalesFactura>
+    </cfdi:Addenda>
+</cfdi:Comprobante>`;
 
         const blob = new Blob([xml], {type: 'text/xml'});
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'tiquet.xml';
+        a.download = 'factura_cfdi_4_0.xml';
         a.click();
         URL.revokeObjectURL(url);
     }
